3da99ac1671f27f9832a8edfcd34b4c2a698c0b3
Bryan Belanger <bbelanger@azcender.com>
2016-07-07 23:40:57 -0400

    Integrate with artifactory_pro module

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,54 @@
-# == Class artifactory_ha::config
+# == Class artifactory::config
 #
-# This class is called from artifactory_ha for service config.
+# This class is called from artifactory for service config.
 #
 class artifactory_ha::config {
+  # Default file sould have artifactory owner and group
+  File {
+    owner => 'artifactory',
+    group => 'artifactory',
+  }
+
+  # Configure cluster home
+  file { $::artifactory_ha::cluster_home:
+    ensure => directory,
+  }
+
+  file { "${::artifactory_ha::cluster_home}/ha-etc":
+    ensure => directory,
+  }
+
+  # Create the plugins directory
+  file { "${::artifactory::clusterhome}/ha-etc/plugins":
+    ensure  => directory,
+  }
+
+  file { "${::artifactory_ha::cluster_home}/ha-data":
+    ensure => directory,
+  }
+
+  file { "${::artifactory_ha::cluster_home}/ha-backup":
+    ensure => directory,
+  }
+
+  # Setup cluster.properties
+  file { "${::artifactory_ha::cluster_home}/cluster.properties":
+    ensure  => file,
+    content => "security.token=${::artifactory_ha::security_token}",
+  }
+
+  file { "${::artifactory::artifactory_home}/etc/ha-node.properties":
+    ensure  => absent,
+    content => epp(
+      'artifactory_ha/ha-node.propertes.epp',
+      {
+        node_id         => $::artifactory_ha::node_id,
+        cluster_home    => $::artifactory_ha::cluster_home,
+        context_url     => $::artifactory_ha::context_url,
+        membership_port => $::artifactory_ha::membership_port,
+        primary         => $::artifactory_ha::primary,
+      }
+    ),
+    mode    => '0644',
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
-# Class: artifactory_ha
+# Class: artifactory
 # ===========================
 #
-# Full description of class artifactory_ha here.
+# Full description of class artifactory here.
 #
 # Parameters
 # ----------
@@ -10,15 +10,51 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
-class artifactory_ha (
-  $package_name = $::artifactory_ha::params::package_name,
-  $service_name = $::artifactory_ha::params::service_name,
-) inherits ::artifactory_ha::params {
 
-  # validate parameters here
+class artifactory_ha(
+  Enum['mssql', 'mysql', 'oracle', 'postgresql'] $db_type,
+  Integer $db_port,
+  String $db_hostname,
+  String $db_username,
+  String $db_password,
+  String $security_token,
+  Boolean $is_primary,
+  String $cluster_home,
+  String $license_key,
+  Integer $membership_port                                                = 10001,
+  String $yum_name                                                        = 'bintray-jfrog-artifactory-pro-rpms',
+  String $yum_baseurl                                                     = 'http://jfrog.bintray.com/artifactory-pro-rpms',
+  String $package_name                                                    = 'jfrog-artifactory-pro',
+  String $artifactory_home                                                = '/var/opt/jfrog/artifactory',
+  Optional[Enum['filesystem', 'fullDb','cachedFS']] $binary_provider_type = undef,
+  Optional[Integer] $pool_max_active                                      = undef,
+  Optional[Integer] $pool_max_idle                                        = undef,
+  Optional[Integer] $binary_provider_cache_maxSize                        = undef,
+  Optional[String] $binary_provider_filesystem_dir                        = undef,
+  Optional[String] $binary_provider_cache_dir                             = undef,
+) {
 
-  class { '::artifactory_ha::install': } ->
-  class { '::artifactory_ha::config': } ~>
-  class { '::artifactory_ha::service': } ->
-  Class['::artifactory_ha']
+  $storage_properties_location = "${cluster_home}/ha-etc/plugins"
+
+  class {'::artifactory_pro::install':
+    license_key                    => $license_key,
+    yum_name                       => $yum_name,
+    yum_baseurl                    => $yum_baseurl,
+    package_name                   => $package_name,
+    artifactory_home               => $artifactory_home,
+    storage_properties_location    => $storage_properties_location,
+    db_type                        => $db_type,
+    db_port                        => $db_port,
+    db_hostname                    => $db_hostname,
+    db_username                    => $db_username,
+    db_password                    => $db_password,
+    binary_provider_type           => $binary_provider_type,
+    pool_max_active                => $pool_max_active,
+    pool_max_idle                  => $pool_max_idle,
+    binary_provider_cache_maxSize  => $binary_provider_cache_maxSize,
+    binary_provider_filesystem_dir => $binary_provider_filesystem_dir,
+    binary_provider_cache_dir      => $binary_provider_cache_dir,
+  } ->
+  class{'::artifactory_ha::config': } ~>
+  Class['::artifactory::service']
 }
