89202d8af8b8de718d16ffa10a25fdfbb538ffb7
Bryan Belanger <bbelanger@azcender.com>
2016-07-08 21:30:43 -0400

    Fix prob

--- manifests/config.pp
+++ manifests/config.pp
@@ -37,19 +37,6 @@ class artifactory_ha::config {
     content => "security.token=${::artifactory_ha::security_token}",
   }
 
-  file { "${::artifactory::artifactory_home}/etc/artifactory.system.properties":
-    ensure  => absent,
-    content => epp(
-      'artifactory_ha/ha-node.properties.epp',
-      {
-        cluster_home    => $::artifactory_ha::cluster_home,
-        membership_port => $::artifactory_ha::membership_port,
-        is_primary      => $::artifactory_ha::is_primary,
-      }
-    ),
-    mode    => '0644',
-  }
-
   file { "${::artifactory::artifactory_home}/etc/ha-node.properties":
     ensure  => file,
     content => epp(
