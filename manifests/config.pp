288b59732f9fa3c781f6d434f740eb8344fe85de
Bryan Belanger <bbelanger@azcender.com>
2016-07-12 09:49:01 -0400

    Fix wget problems

--- manifests/config.pp
+++ manifests/config.pp
@@ -79,5 +79,7 @@ class artifactory_ha::config {
 
   ::wget::fetch { $::artifactory_ha::jdbc_driver_url:
     destination => "${::artifactory::artifactory_home}/tomcat/lib/",
+    mode        => '0775',
+    execuser    => 'artifactory',
   }
 }
