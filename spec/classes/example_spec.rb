2136e6ddef37883a1ac35595c302ecfb0d208489
Bryan Belanger <bbelanger@azcender.com>
2016-07-22 14:05:26 -0400

    Rubocop 2

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe 'artifactory_ha' do
   let(:params) {
-     {
-       :license_key     => 'my_license_key',
-       :jdbc_driver_url => 'http://autostructure.com/my_url',
-       :db_type         => 'mysql',
-       :db_port         => '1001',
-       :db_hostname     => 'db.autostructure.com',
-       :db_username     => 'my_db_user',
-       :db_password     => 'egpiqwgoq[hgewoiehf]',
-       :security_token  => '123security_token_4me',
-       :is_primary      => true,
-       :cluster_home    => '/nfs/home/mnt'
-     }
-   }
+    {
+      license_key: 'my_license_key',
+      jdbc_driver_url: 'http://autostructure.com/my_url',
+      db_type: 'mysql',
+      db_port: '1001',
+      db_hostname: 'db.autostructure.com',
+      db_username: 'my_db_user',
+      db_password: 'egpiqwgoq[hgewoiehf]',
+      security_token: '123security_token_4me',
+      is_primary: true,
+      cluster_home: '/nfs/home/mnt',
+    }
+  }
 
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
@@ -26,7 +26,7 @@ describe 'artifactory_ha' do
         context "artifactory_ha class with minimum configs" do
           it { is_expected.to compile.with_all_deps }
 
-          #it { is_expected.to contain_class('artifactory_pro::params') }
+          # it { is_expected.to contain_class('artifactory_pro::params') }
           it { is_expected.to contain_class('artifactory_pro').that_comes_before('artifactory_ha::config') }
           it { is_expected.to contain_class('artifactory_ha::config').that_comes_before('artifactory_ha::post_config') }
           it { is_expected.to contain_class('artifactory_ha::config') }
